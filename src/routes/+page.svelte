d25d231c1c92ff0f44db293070339ce505102ffe
HaikalAkif <haikalakif17@gmail.com>
2024-01-21 18:22:46 +0800

    🐛 localhost redirect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -76,7 +76,7 @@
 		const params = new URLSearchParams();
 		params.append('client_id', clientId);
 		params.append('response_type', 'code');
-		params.append('redirect_uri', 'http://localhost:5173/callback');
+		params.append('redirect_uri', 'https://are-you-depressed.elyasasmad.com/callback');
 		params.append('scope', 'user-read-private user-read-email');
 		params.append('code_challenge_method', 'S256');
 		params.append('code_challenge', challenge);
@@ -91,7 +91,7 @@
 		params.append('client_id', clientId);
 		params.append('grant_type', 'authorization_code');
 		params.append('code', code);
-		params.append('redirect_uri', 'http://localhost:5173/callback');
+		params.append('redirect_uri', 'https://are-you-depressed.elyasasmad.com/callback');
 		params.append('code_verifier', verifier!);
 
 		const result = await fetch('https://accounts.spotify.com/api/token', {
